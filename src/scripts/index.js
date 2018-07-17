e3224518c3d63c007da98eded2e136fc6d664a74
Mateusz Gorczyca <mateusz.gorczyca@gmail.com>
2018-07-17 19:49:44 +0200

    changed directory tree

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import DataActions from 'flux/actions/DataActions.js';
 
-import Home from 'components/Home.js';
-import About from 'components/About.js';
-import Contact from 'components/Contact.js';
-import Header from 'components/Header.js';
-import News from 'components/News.js'
+import Home from 'components/Home/Home.js';
+import About from 'components/About/About.js';
+import Contact from 'components/Contact/Contact.js';
+import Header from 'components/Header/Header.js';
+import News from 'components/News/News.js'
 
 import {
     BrowserRouter as Router,
